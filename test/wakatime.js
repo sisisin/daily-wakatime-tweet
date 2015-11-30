eb4086d21a81f544edaae99c0ec8c5ae07ac60d1
sisisin <azsisisin@gmail.com>
2015-12-01 00:53:24 +0900

    fix test

--- test/wakatime.js
+++ test/wakatime.js
@@ -4,13 +4,13 @@ const wakatime = require('../lib/wakatime');
 describe('wakatime', () => {
   it('get status 2015/11/21 is 0:17', (done) => {
     wakatime.getGrandTotalTime(new Date('2015/11/21'), (err, time) => {
-      assert(time === '0:17');
+      assert(time === '00:17:27');
       done();
     });
   });
   it('get status 2015/11/20 is 0:00', (done) => {
     wakatime.getGrandTotalTime(new Date('2015/11/20'), (err, time) => {
-      assert(time === '0:00');
+      assert(time === '00:00:00');
       done();
     });
   });
