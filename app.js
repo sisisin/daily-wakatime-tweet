530d6c693a1164ea6b9e9583febd04716a2ecf39
sisisin <azsisisin@gmail.com>
2015-12-02 23:56:25 +0900

    use log4js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict'
 const CronJob = require('cron').CronJob;
 const twitter = require('./lib/twitter');
-
-const job = new CronJob('1 0 0 * * *', twitter, () => { console.log('end');}, false, 'Asia/Tokyo');
+const logger = require('./lib/logger');
+ 
+const job = new CronJob('1 0 0 * * *', twitter, () => logger.info('cron job succeeded.'), false, 'Asia/Tokyo');
 
 job.start();
