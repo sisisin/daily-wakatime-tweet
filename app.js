b187325955b86d6ebd175e9d83cdc98e8431e990
sisisin <azsisisin@gmail.com>
2015-12-02 23:38:05 +0900

    refactor app
    cron's callback move to lib/twitter

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 'use strict'
 const CronJob = require('cron').CronJob;
-
-const wakatime = require('./lib/wakatime');
 const twitter = require('./lib/twitter');
 
-const job = new CronJob('1 0 0 * * *', () => {
-  let yesterday;
-  let t = new Date();
-  t.setDate(t.getDate() - 1);
-  yesterday = new Date(`${t.getFullYear()}/${t.getMonth() + 1}/${t.getDate()}`);
-  
-  wakatime.getGrandTotalTime(yesterday, (err, time) => {
-    twitter(`@Azsimeji ${yesterday.toLocaleDateString()}の作業時間は${time}でした`, (err, data, res) => {
-      if (err) console.log(err);
-      console.log(data);
-    });
-  });  
-}, () => { console.log('end');}, false, 'Asia/Tokyo');
+const job = new CronJob('1 0 0 * * *', twitter, () => { console.log('end');}, false, 'Asia/Tokyo');
 
 job.start();
