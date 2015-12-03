dee1c7638c977fae83f74562a3038a9ebaf59dd7
sisisin <azsisisin@gmail.com>
2015-12-03 23:19:39 +0900

    remove log4js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 const CronJob = require('cron').CronJob;
 const twitter = require('./lib/twitter');
-const logger = require('./lib/logger');
+
  
-const job = new CronJob('1 0 0 * * *', twitter, () => logger.info('cron job succeeded.'), false, 'Asia/Tokyo');
+const job = new CronJob('1 0 0 * * *', twitter, () => console.log('cron job succeeded.'), false, 'Asia/Tokyo');
 
 job.start();
